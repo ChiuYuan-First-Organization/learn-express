741b3ece7f64253c6235f935ceb99d44873715e5
ChiuYuan0214 <rumble88481@gmail.com>
2022-01-30 15:52:41 +0800

    add routes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
 
-const server = http.createServer(app);
 
-server.listen(3000);
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
