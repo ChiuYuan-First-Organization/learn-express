545d6f1f4cf2691467ec959c177d0205eeec5b4e
ChiuYuan0214 <rumble88481@gmail.com>
2022-01-30 15:58:24 +0800

    add error page

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use(adminRoutes);
-
+// Error page
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found.</h1>');
+});
 
 // const server = http.createServer(app);
 
