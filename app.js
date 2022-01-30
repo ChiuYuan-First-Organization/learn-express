c29befae0cdb172ebd7fe6a5a6fdcadc2e270cd9
ChiuYuan0214 <rumble88481@gmail.com>
2022-01-30 22:47:27 +0800

    add template HTML

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+// tell node to use pug as templating engine
+app.set('view engine', 'pug');
+// set the value of 'views' to the folder diection which contains the templates
+app.set('views', 'views');
 
 // to parse the request body
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,12 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Page routes
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // Error page
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.render('404');
 });
 
 // const server = http.createServer(app);
